1cba2fa479715aabd98dd5548377bd274457e884
svc <svcqa1@celitotech.com>
2023-08-03 14:36:44 +0530

    08/03/2023 14:36:44

---
+++ cron.ps1
@@ -0,0 +1,8 @@
+cd C:\Users\team4\Documents\Docker-practise
+date
+history >> History.txt
+git add .
+git status
+git commit -m "$(date)"
+#assuming ssh keys anre configured in github t avoid password prompt and also set the git remote origin as required repo url in local
+git push 
